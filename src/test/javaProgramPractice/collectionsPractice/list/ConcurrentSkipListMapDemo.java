7b0b688cbc06c214c018a4de2fa5123ac5585489
sanjeev763 <sanjeev76326510@gmail.com>
2025-06-22 00:58:10 +0530

    ConcurrentSkipListMap example

---
+++ src/test/javaProgramPractice/collectionsPractice/list/ConcurrentSkipListMapDemo.java
@@ -0,0 +1,12 @@
+package collectionsPractice.list;
+//Thread safe tree map
+//skipList - probabilistic data structure that allows for efficient search, insertion, and deletion operation
+//Similar to sorted linked list but with multiple layers that skip over portions of the list to provide faster access to elements
+// layer 3 : 1,......5,........9
+// layer 2 : 1,.....,3,....5,.....7,....,9
+// layer 1 : 1,2,3,4,5,6,7,8,9
+public class ConcurrentSkipListMapDemo {
+    public static void main(String[] args) {
+
+    }
+}
